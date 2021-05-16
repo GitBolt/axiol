30b11dd271679918a9aa54429d2da2d423c129a2
GitBolt <stingbolt1@gmail.com>
2021-05-16 21:47:38 +0530

    Server Prefixes

---
+++ README.md
@@ -0,0 +1,14 @@
+<p align="center">
+  <a href="https://discord.com/api/oauth2/authorize?client_id=843484459113775114&permissions=8&scope=bot">
+    <img
+      alt="Axiol"
+      src="https://cdn.discordapp.com/attachments/843519647055609856/843519687148044288/Bg.png"
+      width="400"
+    />
+  </a>
+</p>
+
+Axiol is a discord bot coded in pure python, it is a multipurpose advanced bot containing most features for which people require seperate bots.
+For any suggestions, the command `suggest` can be used (starting with the server prefix. Default is `#`) and the suggestion will be sent to the
+[Offical Discord Support Server](https://discord.gg/KTn4TgwkUT)
+
--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 import os
 import discord
-import json
+import asyncio
 from discord.ext import commands
 from pymongo import MongoClient
+import emojis
 
-#getting server prefixes
-def serverprefixes(client, message):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
-    return prefixes[str(message.guild.id)]
+#Database Fetching
+MONGOCLIENT = MongoClient(os.environ.get("MONGO_URL"))
+DATABASE = MONGOCLIENT['Axiol']
+PREFIXES = DATABASE["Prefixes"] #Prefixes Collection
 
-bot = commands.Bot(command_prefix = serverprefixes)
+
+#Getting Server Prefix
+def serverprefix(bot, message):
+    if PREFIXES.find_one({"serverid": message.guild.id}) is None:
+        return "#"
+    return PREFIXES.find_one({"serverid": message.guild.id}).get("prefix")
+    
+bot = commands.Bot(command_prefix = serverprefix)
 bot.remove_command('help')
 
 @bot.event
@@ -19,30 +26,40 @@ async def on_ready():
     print("I'm Ready!")
 
 
-#simple json file prefixes, soon to be switched to mongodb
-@bot.event
-async def on_guild_join(guild):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
-    prefixes[str(guild.id)] = "#"
-    with open("prefixes.json", "w") as f:
-        json.dump(prefixes, f)
-
 @bot.command()
 @commands.has_permissions(administrator = True)
-async def changeprefix(ctx, prefix):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
-    prefixes[str(ctx.guild.id)] = prefix
+async def prefix(ctx):
+    embed = discord.Embed(title="Prefix for this server",
+                        description=f"Hey, the prefix for this server is `#`\n Wanna change it? React to the {emojis.SETTINGS} emoji below!",
+                        color=discord.Color.teal())
+    message = await ctx.send(embed=embed)
+    await message.add_reaction(emojis.SETTINGS)
+    await asyncio.sleep(1)
 
-    with open("prefixes.json", "w") as f:
-        json.dump(prefixes, f)
+    def reactioncheck(reaction, user):
+        return str(reaction.emoji) == emojis.SETTINGS and reaction.message == message and reaction.me == True
+    react = await bot.wait_for('reaction_add', check=reactioncheck)
+    if react:  
+        await ctx.send(embed=discord.Embed(description="Send the prefix and I will change it, don't send anything, *just your prefix*", color=discord.Color.purple()))
+
+    def prefixmsgcheck(message):
+        return message.author == ctx.author
+    try:
+        userprefix = await bot.wait_for('message', check=prefixmsgcheck, timeout=60.0)
+        if PREFIXES.find_one({"serverid": message.guild.id}) is None:
+            PREFIXES.insert_one({"_id": PREFIXES.estimated_document_count()+1, "serverid": ctx.author.guild.id, "prefix": userprefix.content})
+        oldprefix = PREFIXES.find_one({"serverid": message.guild.id})
+        newprefix = {"$set": {"serverid": message.guild.id, "prefix": userprefix.content}}
+        PREFIXES.update_one(oldprefix, newprefix)
+        await ctx.send(f"Updated your new prefix, it's `{userprefix.content}` if I am not wrong :eyes:")
+    except asyncio.TimeoutError:
+        await ctx.author.send("You took too long to enter your prefix ⏲")
 
 
-#loading cogs
+#Loading Cogs
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
 
-#starting bot
+#Starting Bot
 bot.run(os.environ.get("TOKEN"))

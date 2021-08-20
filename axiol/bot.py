efd79eea9d551f97946affedb84a8e75a90008f3
Yohann Boniface <edhyjox@gmail.com>
2021-08-20 21:59:07 +0200

    added basic starting logs

--- axiol/bot.py
+++ axiol/bot.py
@@ -1,22 +1,33 @@
+"""Axiol Bot Core."""
+
 import os
 import dotenv
 
 from discord.ext import commands
+from classes.logging import log
 
 TOKEN_KEY: str = 'TOKEN'
 
 
 class Bot(commands.Bot):
+    """Axiol custom bot class."""
 
-    def __init__(self):
+    def __init__(self) -> None:
+        log.inform("Initializing bot...")
+
         super(Bot, self).__init__(command_prefix=',')
         self.remove_command('help')
 
-    def run(self):
+    def run(self) -> None:
+        log.inform("Starting bot...")
+
         super(Bot, self).run(
             os.environ.get(TOKEN_KEY)
             or dotenv.dotenv_values('.env').get(TOKEN_KEY)
         )
 
-    async def on_connect(self):
-        print("Logged int as", self.user)
+    async def on_connect(self) -> None:
+        log.success(f"Logging in as {self.user}.")
+
+    async def on_ready(self) -> None:
+        log.info(f"{self.user} is ready for use.")

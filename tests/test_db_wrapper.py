56094b62d226e6752231f485df2433e0775d9198
Yohann Boniface <edhyjox@gmail.com>
2021-08-21 07:10:40 +0200

    🔧 adding __all__ and removed else

---
+++ tests/test_db_wrapper.py
@@ -0,0 +1,35 @@
+import sys
+import unittest
+from contextlib import contextmanager
+from io import StringIO
+
+
+from axiol.classes.db_wrapper import Database, Collections
+
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_instanciation(self):
+        for cls in (Collections, Database):
+            with captured_output() as (out, err):
+                cls(None)
+
+            output = out.getvalue().strip()
+            self.assertTrue('Warning' in output)
+            print(output)
+
+
+if __name__ == '__main__':
+    unittest.main()

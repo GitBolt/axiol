f263b1a07c634e037abf50e37875c5c32f5d87eb
Yohann Boniface <edhyjox@gmail.com>
2021-08-21 08:30:21 +0200

    🔧 adding DB_PATH to __init__.py

--- axiol/__init__.py
+++ axiol/__init__.py
@@ -1,1 +1,12 @@
+import os
+
 __version__: str = '2.0.0'
+
+DOTENV_PATH: str = os.path.abspath(
+    os.path.join(
+        __file__,
+        os.path.pardir,
+        os.path.pardir,
+        '.env'
+    )
+)

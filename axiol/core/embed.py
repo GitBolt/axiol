77f66a7baa860d50e4fc3dbe6ad05ac1d736e0b1
Yohann Boniface <edhyjox@gmail.com>
2021-08-25 17:59:46 +0200

    🔧 Moving set_footer to embed update method

--- axiol/core/embed.py
+++ axiol/core/embed.py
@@ -8,9 +8,15 @@ import embed_templator
 class Embed(embed_templator.Embed):
 
     def setup(self) -> Embed:
+        return self.set_author(
+            name=f"Requested by {self.ctx.author} 🚀",
+            icon_url=self.ctx.author.avatar_url
+        )
+
+    def update(self) -> None:
         lucky: str = (
-            "There was 1 / 1 000 000 chance for this message to show 🍀"
-        ) * (not random.randint(0, 1_000_000))
+            "There was 1 / 10 000 chance for this message to show 🍀"
+        ) * (not random.randint(0, 10_000))
 
         self.set_footer(
             icon_url=self.client.user.avatar_url,
@@ -21,9 +27,4 @@ class Embed(embed_templator.Embed):
                     f"🔑 {self.ctx.prefix}help",
                 )
             )
-        )
-
-        return self.set_author(
-            name=f"Requested by {self.ctx.author} 🚀",
-            icon_url=self.ctx.author.avatar_url
         )

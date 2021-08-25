6dd486f4ff612ebb2bdecbd357da3a714dc86e91
Yohann Boniface <edhyjox@gmail.com>
2021-08-25 21:36:31 +0200

    🔧 Moving captured output to functions

---
+++ axiol/utils/functions/__init__.py
@@ -0,0 +1,3 @@
+from captured_output import captured_output
+
+__all__ = ('captured_output',)
---
+++ axiol/utils/functions/captured_output.py
@@ -0,0 +1,20 @@
+import sys
+from contextlib import contextmanager
+from captured_output import StringIO
+from typing import Tuple
+
+
+# Credits to: Rob Kennedy
+# https://stackoverflow.com/questions/4219717/
+
+@contextmanager
+def captured_output() -> Tuple[StringIO, StringIO]:
+    """Capture stdout, stderr and returns a tuple of their contents."""
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err

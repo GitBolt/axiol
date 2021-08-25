b9cbf5b1e4ed9c94ac1dbc94467f8019c69660ab
Yohann Boniface <edhyjox@gmail.com>
2021-08-25 20:16:07 +0200

    ✨ Adding DB based prefix

--- axiol/core/bot.py
+++ axiol/core/bot.py
@@ -8,9 +8,10 @@ from discord import Message
 from discord.ext import commands
 
 from axiol import DOTENV_PATH, PREVENT_DOUBLE_RUNTIME_ERROR
-from axiol.utils.logger import log
 from axiol.core.context import TimedContext
+from axiol.database.db_wrapper import collections
 from axiol.tasks.update_presence import update_presence
+from axiol.utils.logger import log
 
 TOKEN_KEY: str = 'TOKEN'
 
@@ -21,7 +22,8 @@ class Bot(commands.Bot):
     def __init__(self, prefix: str) -> None:
         log.inform("Initializing bot...")
 
-        super(Bot, self).__init__(command_prefix=prefix)
+        self.default_prefix = prefix
+        super(Bot, self).__init__(command_prefix=self.get_prefix)
         self.remove_command('help')
         log.inform("Loaded Embed Templator")
 
@@ -39,7 +41,7 @@ class Bot(commands.Bot):
         )
 
     def load_extension(self, name: str, *, _package=None) -> None:
-        cog_path: str = f'cogs.{name}'
+        cog_path: str = f'axiol.cogs.{name}'
         cog_name: str = cog_path.split('.')[1]
 
         log.inform(f"Loading {cog_name} extension...")
@@ -55,6 +57,21 @@ class Bot(commands.Bot):
 
         else:
             log.success(f"loaded {cog_name}")
+
+    async def get_prefix(self, message):
+        """Return current guild prefix"""
+        if not message.guild:
+            return self.default_prefix
+
+        prefix_doc = (
+            await collections.prefixes
+            .find_one({"_id": message.guild.id})
+        )
+
+        if prefix_doc is None:
+            return self.default_prefix
+
+        return prefix_doc.get("prefix", self.default_prefix)
 
     async def on_connect(self) -> None:
         log.success(f"Logging in as {self.user}.")
